110d1fb64152750ff0b56ca3265f8e1774e2b843
HossamAshraf909 <ho.ashraf9090@gmail.com>
2025-03-23 14:00:37 +0200

    add product and order_prod

--- Shipping.DAL/obj/Debug/net9.0/Shipping.DAL.AssemblyInfo.cs
+++ Shipping.DAL/obj/Debug/net9.0/Shipping.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shipping.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49527a7fd9194c518eb265c35ca3f9961b90e813")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46bff68560e89167b6655034ff8c7ec9a109cd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shipping.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shipping.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
