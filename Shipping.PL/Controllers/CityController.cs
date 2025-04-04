5bacb3dfe1100afa6fb9128dca05b0c906cad90a
HossamAshraf909 <ho.ashraf9090@gmail.com>
2025-04-04 22:05:24 +0200

    add Update to OrderController

--- Shipping.PL/Controllers/CityController.cs
+++ Shipping.PL/Controllers/CityController.cs
@@ -23,6 +23,43 @@ namespace Shipping.PL.Controllers
             await cityService.AddAsync(city);
             return Ok();
         }
-
+        [HttpGet]
+        public async Task<IActionResult> GetAllCities()
+        {
+            var cities = await cityService.GetAllAsync();
+            if (cities == null) return NotFound();
+            return Ok(cities);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCityById(int id)
+        {
+            var city = await cityService.GetByIdAsync(id);
+            if (city == null) return NotFound();
+            return Ok(city);
+        }
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateCity(int id, AddCityDTO city)
+        {
+            if (city == null) return BadRequest();
+            if (!ModelState.IsValid) return BadRequest();
+            await cityService.UpdateAsync(id, city);
+            return Ok();
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await cityService.GetByIdAsync(id);
+            if (city == null) return NotFound();
+            await cityService.DeleteAsync(id);
+            return Ok();
+        }
+        [HttpGet("search")]
+        public async Task<IActionResult> Search(string searchword)
+        {
+            if (string.IsNullOrEmpty(searchword)) return BadRequest();
+            var cities = await cityService.Search(searchword);
+            if (cities == null) return NotFound();
+            return Ok(cities);
+        }
     }
 }
