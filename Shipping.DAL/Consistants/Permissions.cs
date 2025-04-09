99c6d33434dd557bdbd821af7845c6475fe6b402
HossamAshraf909 <ho.ashraf9090@gmail.com>
2025-04-09 21:54:44 +0200

    Add GetOrderByID

--- Shipping.DAL/Consistants/Permissions.cs
+++ Shipping.DAL/Consistants/Permissions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shipping.BL.Consistants
+namespace Shipping.DAL.Consistants
 {
     public static class Permissions
     {
