a70449392640f3038f4c284ff15eb6836d597f44
HossamAshraf909 <ho.ashraf9090@gmail.com>
2025-04-13 18:23:23 +0200

    Update My Brach

--- Shipping.DAL/obj/Debug/net9.0/Shipping.DAL.AssemblyInfo.cs
+++ Shipping.DAL/obj/Debug/net9.0/Shipping.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shipping.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2857790114f2b9cb846bea8c121c15fc1db81b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5ac58faaa047161485aa9ce49739cbf946fc61")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shipping.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shipping.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Shipping.PL/obj/Debug/net9.0/Shipping.PL.AssemblyInfo.cs
+++ Shipping.PL/obj/Debug/net9.0/Shipping.PL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shipping.PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2857790114f2b9cb846bea8c121c15fc1db81b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5ac58faaa047161485aa9ce49739cbf946fc61")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shipping.PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shipping.PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
